76991ec90ccb1a6a68067f30bb3218bf42f75847
Michael Johann <mjohann@rails-experts.com>
2016-02-22 11:21:53 +0100

    added alias for db:migrate

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -167,6 +167,7 @@ alias todo='open nvalt://find/todo'
 alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
 
 alias rdm='rake db:migrate'
+alias berdm='bundle exec rake db:migrate'
 alias rdmr='rake db:migrate:redo'
 
 # Zeus
